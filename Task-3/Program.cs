2a5145000a27cbf446864b7449ff0ac39a2e43f5
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-29 04:24:01 +0700

    Initial commit

---
+++ Task-3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countAllImages = 52;
+            int countMaxImagesOnPage = 3;
+            int fullPagesWithImages = countAllImages / countMaxImagesOnPage;
+            int leftImages = countAllImages % countMaxImagesOnPage;
+
+            Console.WriteLine($"Страниц заполненых картинками: {fullPagesWithImages} \n" +
+                $"Картинок сверх меры: {leftImages}");
+            Console.ReadKey();
+        }
+    }
+}
